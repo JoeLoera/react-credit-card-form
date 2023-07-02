406e71cedb1b48e4beddd5c9861525b8b3e370f1
Joe Loera <joeloera7@gmail.com>
2023-07-02 10:05:16 -0700

    Completed Credit Card App

--- src/App.js
+++ src/App.js
@@ -1,55 +1,108 @@
-import CreditCard from './components/CreditCard' 
-const App = () => {
+import { useState } from 'react'
 
-  const handleSubmit = () => {
-    console.log("submitted!")
+import CreditCard from './components/CreditCard'
+
+const App = () => {
+  const currentMonth = new Date().getMonth() + 1
+  const formatting = String(currentMonth).length <= 1 ? '0' : null
+  const formattedCurrentMonth = formatting + currentMonth
+  const currentYear = new Date().getFullYear()
+
+  const [formData, setFormData] = useState({
+    name: '',
+    number: '',
+    date: currentYear + '-' + formattedCurrentMonth,
+    cvv: '',
+  })
+  const [side, setSide] = useState('front')
+  const [message, setMessage] = useState(
+    'Please enter your credit card details'
+  )
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    let value = e.target.value
+
+    if (name === 'cvv' || name === 'number') {
+      value = value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1')
+    }
+
+    setFormData({ ...formData, [name]: value })
+
+    if (name === 'cvv') {
+      setSide('back')
+      return
+    }
+    setSide('front')
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMessage('Thank you for your custom')
   }
 
   return (
     <div className="form-container">
-      <form onSubmit ={handleSubmit}>
+      <CreditCard formData={formData} side={side} />
+      <form onSubmit={handleSubmit}>
         <div className="input-container">
-          <label>Name on card
+          <label>
+            Name on card
             <input
-            placeholder="Name"
-            required
+              name="name"
+              value={formData.name}
+              placeholder="Name"
+              required
+              onChange={handleChange}
             />
           </label>
         </div>
         <div className="input-container">
-          <label>Card number
+          <label>
+            Card number
             <input
-            placeholder="0000 0000 0000 0000"
-            min-length={16}
-            max-length={16}
-            required
+              name="number"
+              value={formData.number}
+              placeholder="0000 0000 0000 0000"
+              minLength={16}
+              maxLength={16}
+              required
+              onChange={handleChange}
             />
           </label>
         </div>
         <div className="supporting-inputs-container">
           <label>
             Expiry date
-            <input type="month"
-            required
+            <input
+              name="date"
+              value={formData.date}
+              type="month"
+              required
+              onChange={handleChange}
             />
           </label>
           <label>
             CVV
-            <input 
-            id="cvv"
-            placeholder="123"
-            min-length={3}
-            max-length={3}
+            <input
+              name="cvv"
+              value={formData.cvv}
+              id="cvv"
+              placeholder="123"
+              minLength={3}
+              maxLength={3}
+              required
+              onChange={handleChange}
             />
           </label>
         </div>
         <div className="input-container">
-            <input type="submit"/>
+          <input type="submit" />
         </div>
-        <p className="info-message">ddd</p>
+        <p className="info-message">{message}</p>
       </form>
     </div>
   )
 }
 
-export default App;
+export default App
